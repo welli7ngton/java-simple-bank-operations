1b84d2f10e82135aaa5690895e18f6c826f8c852
welli7ngton <wellingtonasilva45@gmail.com>
2024-03-19 19:54:11 -0300

    ultimos ajustes nos menus

--- src/Main.java
+++ src/Main.java
@@ -9,49 +9,49 @@ public class Main {
     public static int loggedCustomerCode;
     public static boolean isLogged = false;
     private static final Scanner scMain = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Choose one option: [1]Login [2]Register [3]Cancel");
-        int userInput = scMain.nextInt();
+        while (true) {
+            System.out.println("Choose one option: [1]Login [2]Register [3]Cancel");
+            int userInput = scMain.nextInt();
+            scMain.nextLine();
 
-        if (userInput == 1 && !isLogged){
-            login();
-            isLogged = true;
-        } else if (userInput == 2 && !isLogged){
-            loggedCustomerCode = CadastroCliente.registerClient();
-            isLogged = true;
-        } else if (userInput == 3){
-            System.out.println("Program closed.");
-        }
-
-        if (isLogged){
-            System.out.println("LOGGED AS - " + loggedCustomer.get("Name"));
-            System.out.println("Choose one option: [1]Change Register [2]Others Bank operations:");
-            userInput = scMain.nextInt();
-            if (userInput == 1){
-                AlteraDadosCliente.getClientToChangeData();
-            }
-        }
-
-    }
-
-    public static void login(){
-        System.out.println("=====LOGIN=====");
-        System.out.println("Type your 'Account Code':");
-        int accountCode = scMain.nextInt();
-        if (accountCode <= allBankUsers.size()){
-            HashMap<String, Object> customer = allBankUsers.get(accountCode);
-            System.out.println("Type you password:");
-            String passwd = scMain.next();
-            if (Objects.equals(passwd, (String) customer.get("passwd"))){
-                System.out.println("Login Successful.");
-                loggedCustomerCode = accountCode;
+            if (userInput == 1 && !isLogged) {
+                Login.login();
                 isLogged = true;
-                loggedCustomer = customer;
-            } else {
-                System.out.println("Login Failed: wrong password.");
+            } else if (userInput == 2 && !isLogged) {
+                loggedCustomerCode = CadastroCliente.registerClient();
+                isLogged = true;
+            } else if (userInput == 3) {
+                isLogged = false;
+                loggedCustomer = null;
+                loggedCustomerCode = 0;
+                System.out.println("Program closed.");
+                break;
             }
-        } else {
-            System.out.println("Login Failed 404: Account Code not found.");
+
+            if (isLogged) {
+                while (true) {
+                    System.out.println("LOGGED AS - " + loggedCustomer.get("Name"));
+                    System.out.println("Choose one option: [1]Change Register [2]Show Account info [3]Others Bank operations [4]Logout:");
+                    int userInput2 = scMain.nextInt();
+                    scMain.nextLine();
+                    if (userInput2 == 1) {
+                        AlteraDadosCliente.getClientToChangeData();
+                    } else if (userInput2 == 2) {
+                        CadastroCliente.showClientData();
+                    } else if (userInput2 == 3) {
+                        OperacoesBancarias.showBankOperationsMenu();
+                    } else if (userInput2 == 4) {
+                        loggedCustomer = null;
+                        loggedCustomerCode = 0;
+                        isLogged = false;
+                        break;
+                    } else {
+                        System.out.println("Invalid input. Please enter a valid option.");
+                    }
+                }
+            }
         }
     }
 }
