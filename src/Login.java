f9c19caa7a7de6d85b6c1e233a6b449c836b32b2
welli7ngton <wellingtonasilva45@gmail.com>
2024-03-19 19:54:39 -0300

    ultimos ajustes login

--- src/Login.java
+++ src/Login.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Login {
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Login {
+    private static final Scanner scLogin = new Scanner(System.in);
+    public static void login(){
+        System.out.println("=====LOGIN=====");
+        System.out.println("Type your 'Account Code':");
+        int accountCode = scLogin.nextInt();
+        if (accountCode <= Main.allBankUsers.size()){
+            HashMap<String, Object> customer = Main.allBankUsers.get(accountCode);
+            System.out.println("Type you password:");
+            String passwd = scLogin.next();
+            if (Objects.equals(passwd, (String) customer.get("passwd"))){
+                System.out.println("Login Successful.");
+                Main.loggedCustomerCode = accountCode;
+                Main.isLogged = true;
+                Main.loggedCustomer = customer;
+            } else {
+                System.out.println("Login Failed: wrong password.");
+            }
+        } else {
+            System.out.println("Login Failed 404: Account Code not found.");
+        }
+    }
 }
